302d0d273d683b28dc9474149e2141f035c1683e
Aniket Tiwari <anikettiwari@Anikets-MacBook-Pro.local>
2023-10-11 19:54:14 -0400

    added view panel and form panel

---
+++ src/Model/Patient.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author anikettiwari
+ */
+public class Patient {
+    
+    private String firstName;
+    private String lastName;
+    private String ageText; 
+    private String email;
+    private String patientType;
+    private String gender;
+    private String message;
+    private Date date;
+
+   
+    public Patient(){
+        
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAgeText() {
+        return ageText;
+    }
+
+    public void setAgeText(String ageText) {
+        this.ageText = ageText;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPatientType() {
+        return patientType;
+    }
+
+    public void setPatientType(String patientType) {
+        this.patientType = patientType;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+     public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+    
+}
